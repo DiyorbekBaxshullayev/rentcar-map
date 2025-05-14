007e8ae6499c85a89208ff4eaf224dd161d1a010
DiyorbekBaxshullayev <baxshullayevd4@gmail.com>
2025-05-14 15:21:26 +0500

    .

---
+++ map/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// src/pages/Home.jsx
+import Map from "../components/Map";
+
+const Home = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <Map />
+    </div>
+  );
+};
+
+export default Home;
